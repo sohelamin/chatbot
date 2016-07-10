2436fd9020524c4571b7fc2fdae0ddfe085cea7d
Sohel Amin <sohelamincse@gmail.com>
2016-07-10 14:54:12 +0600

    Python raw file added

---
+++ main-raw.py
@@ -0,0 +1,21 @@
+import aiml
+import os
+
+kernel = aiml.Kernel()
+
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+# kernel now ready for use
+while True:
+    message = raw_input("Enter your message to the bot: ")
+    if message == "quit":
+        exit()
+    elif message == "save":
+        kernel.saveBrain("bot_brain.brn")
+    else:
+        bot_response = kernel.respond(message)
+        print bot_response
